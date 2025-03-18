6b7595b11aa057314134a62e85589c73c687279a
Danhdev98 <danh.vo.2110@gmail.com>
2025-03-19 00:40:51 +0700

    verify-email

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ import authRoutes from "./routers/auth.routes.js"
 import dotenv from "dotenv"
 import { connectDB } from "./libs/db.js"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config()
 const PORT = process.env.PORT
 const app = express()
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
